56e39b7045597e258a100e7a32a76dd13a34e739
Timur U <ucomru@pm.me>
2024-03-23 19:24:04 +0300

    Python venv added

--- options.lua
+++ options.lua
@@ -1,10 +1,15 @@
-
-return {
-
+-- :let -- get all var
+return {  
   opt = { -- vim.opt.
-    list = true,
+    -- list = true,
     listchars = { tab = ">·", eol = "·" },
     colorcolumn = "80",
   },
-
+  g = { -- vim.g.
+    -- python3 -m venv .venv && source .venv/bin/activate && pip install pynvim
+    -- :lua =vim.b.gitsigns_status_dict['root'] -- package root path
+    -- :echo g:python3_host_prog | :lua =vim.g.python3_host_prog
+    -- :lua vim.g.python3_host_prog=vim.fn.exepath('python')
+    python3_host_prog = "~/.venv/bin/python",
+  },
 }
