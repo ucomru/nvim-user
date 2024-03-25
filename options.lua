ec8c95b487a53c1ba78358feeb5f857168b3c45d
Timur U <ucomru@pm.me>
2024-03-25 17:36:44 +0300

    Added `vim.fn.expand` for the `~`

--- options.lua
+++ options.lua
@@ -1,15 +1,14 @@
 -- :let -- get all var
-return {  
+-- :checkhealth
+return {
+  g = { -- vim.g.
+    -- :lua =vim.b.gitsigns_status_dict["root"] -- package root path
+    -- :echo g:python3_host_prog | :lua =vim.g.python3_host_prog
+    python3_host_prog = vim.fn.expand("~/.venv/bin/python"),
+  },
   opt = { -- vim.opt.
     -- list = true,
     listchars = { tab = ">·", eol = "·" },
     colorcolumn = "80",
-  },
-  g = { -- vim.g.
-    -- python3 -m venv .venv && source .venv/bin/activate && pip install pynvim
-    -- :lua =vim.b.gitsigns_status_dict['root'] -- package root path
-    -- :echo g:python3_host_prog | :lua =vim.g.python3_host_prog
-    -- :lua vim.g.python3_host_prog=vim.fn.exepath('python')
-    python3_host_prog = "~/.venv/bin/python",
   },
 }
